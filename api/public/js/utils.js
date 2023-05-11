d9f22e54edf6b020392cbac439b577b5f970b377
Daniel Lucas Pessoa Monteiro dos Santos <daniellucas-pms@hotmail.com>
2023-05-11 09:51:29 -0300

    refactor: improving steps list

--- api/public/js/utils.js
+++ api/public/js/utils.js
@@ -2,54 +2,47 @@ import { addAnalysisCard, disableAnalyseButton, toogleAnalyseButtonLoading, setT
 import { getAnalysis } from "./api.js";
 
 const analysisSteps = {
-  ARTICLE: "Processar conteúdo da página",
-  GOOGLE_QUERY: "Gerar query de busca",
-  GOOGLE_SEARCH_RESULTS: "Buscar no google",
-  FAKENO_AI_PREDICTION: "Classificar com a FakeNo-AI",
-  GPT_ANALYSIS: "Gerar análise com o GPT"
+  ARTICLE: {
+    description: "Processar conteúdo da página",
+    executionTime: null
+  },
+  GOOGLE_QUERY: {
+    description: "Gerar query de busca",
+    executionTime: null
+  },
+  GOOGLE_SEARCH_RESULTS: {
+    description: "Buscar no google",
+    executionTime: null
+  },
+  FAKENO_AI_PREDICTION: {
+    description: "Classificar com a FakeNo-AI",
+    executionTime: null
+  },
+  GPT_ANALYSIS: {
+    description: "Gerar análise com o GPT",
+    executionTime: null
+  },
 }
 
 export async function handleAnalyseButtonClick() {
   await toogleAnalyseButtonLoading();
   
+  let stepTime = Date.now();
   const response = await getAnalysis();
   
   const reader = response.body.getReader() 
   const decoder = new TextDecoder();
   
   let analysis = "";
-  let stepTime = Date.now();
-  const steps = {
-    ARTICLE: {
-      description: "Processar conteúdo da página",
-      executionTime: null
-    },
-    GOOGLE_QUERY: {
-      description: "Gerar query de busca",
-      executionTime: null
-    },
-    GOOGLE_SEARCH_RESULTS: {
-      description: "Buscar no google",
-      executionTime: null
-    },
-    FAKENO_AI_PREDICTION: {
-      description: "Classificar com a FakeNo-AI",
-      executionTime: null
-    },
-    GPT_ANALYSIS: {
-      description: "Gerar análise com o GPT",
-      executionTime: null
-    },
-  }
 
   reader.read().then(function process({ done, value }) {
     if (done) return;
   
     const step = JSON.parse(decoder.decode(value))
 
-    steps[step.title].executionTime = Date.now() - stepTime;
+    analysisSteps[step.title].executionTime = Date.now() - stepTime;
     setTooltipContent(
-      createStepsList(steps)
+      createStepsList(analysisSteps)
     )
     stepTime = Date.now();
 
@@ -73,12 +66,12 @@ function createStepsList(steps) {
     const step = steps[stepKey];
     const icon = step.executionTime ? '✅' : '➖';
     const time = step.executionTime ? `+${step.executionTime}ms` : ''
-    return `${icon} ${step.description}... ${time}`
+    return `<li>${icon} ${step.description}... ${time}</li>`
   })
   
   return `
     <ul>
-      ${fomattedSteps.join('<br/>')}
+      ${fomattedSteps.join('\n')}
     </ul>
   `
 }
